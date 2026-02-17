ff53e92705aff16fdc2e0cd8110889dc38355d51
simonwang <simon.wang@agr.gc.ca>
2026-02-17 16:34:08 -0500

    add connection upgrade

--- managed-containers/nginx/99-fsdh.sh
+++ managed-containers/nginx/99-fsdh.sh
@@ -5,6 +5,7 @@ sed -i "s/80;/$FSDH_PORT;/g" /etc/nginx/conf.d/default.conf
 
 # create proxy
 sed -i "/location \//a \        proxy_set_header $FSDH_USER_HEADER_NAME $FSDH_USER_HEADER_DEFAULT;" /etc/nginx/conf.d/default.conf 
+sed -i "/location \//a \        proxy_set_header Connection upgrade;" /etc/nginx/conf.d/default.conf 
 sed -i "/location \//a \        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;" /etc/nginx/conf.d/default.conf 
 sed -i "/location \//a \        proxy_set_header X-Real-IP \$remote_addr;" /etc/nginx/conf.d/default.conf 
 sed -i "/location \//a \        proxy_set_header Host \$host;" /etc/nginx/conf.d/default.conf 
