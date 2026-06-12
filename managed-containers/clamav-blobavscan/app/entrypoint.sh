c78e56f84f567bdb3d1aadf9febb0319787759bc
John Bain <John.Bain@gmail.com>
2026-06-12 19:02:12 +0000

    fixed uname issue and entrypoint

--- managed-containers/clamav-blobavscan/app/entrypoint.sh
+++ managed-containers/clamav-blobavscan/app/entrypoint.sh
@@ -7,7 +7,4 @@ freshclam --config-file=/etc/clamav/freshclam.conf --stdout
 
 clamd --config-file=/etc/clamav/clamd.conf
 
-# shellcheck disable=SC1091
-. /opt/venv/bin/activate
-
 python3 scan_blob.py
